af1774ce25e1d3713d2d706cff71e871661e6cf2
b-long <b-long@users.noreply.github.com>
2025-06-11 09:49:12 -0400

    Move languages to distinct file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,6 +13,7 @@ import type * as OpenApiPlugin from "docusaurus-plugin-openapi-docs";
 import { openApiSpecs, preprocessOpenApiSpecs } from "./preprocessing"
 import { ApiPageMetadata, SchemaPageMetadata, SidebarOptions } from "docusaurus-plugin-openapi-docs/src/types";
 import type * as Plugin from "@docusaurus/types/src/plugin";
+import languageTabs from "./openapi-generated-sdks";
 
 // --- OpenAPI Config Helper ---
 // We'll merge 'openApiDocsConfig' with 'sampleConfig' later
@@ -264,99 +265,7 @@ const config: Config = {
       // },
     },
 
-    languageTabs: [
-      {
-        highlight: "python",
-        language: "python",
-        logoClass: "python",
-      },
-      {
-        highlight: "bash",
-        language: "curl",
-        logoClass: "curl",
-      },
-      {
-        highlight: "csharp",
-        language: "csharp",
-        logoClass: "csharp",
-      },
-      // {
-      //   highlight: "go",
-      //   language: "go",
-      //   logoClass: "go",
-      // },
-      // {
-      //   highlight: "javascript",
-      //   language: "nodejs",
-      //   logoClass: "nodejs",
-      // },
-      {
-        highlight: "ruby",
-        language: "ruby",
-        logoClass: "ruby",
-      },
-      {
-        highlight: "php",
-        language: "php",
-        logoClass: "php",
-      },
-      // {
-      //   highlight: "java",
-      //   language: "java",
-      //   logoClass: "java",
-      //   variant: "unirest",
-      // },
-      {
-        highlight: "powershell",
-        language: "powershell",
-        logoClass: "powershell",
-      },
-      {
-        highlight: "dart",
-        language: "dart",
-        logoClass: "dart",
-      },
-      // {
-      //   highlight: "javascript",
-      //   language: "javascript",
-      //   logoClass: "javascript",
-      // },
-      {
-        highlight: "c",
-        language: "c",
-        logoClass: "c",
-      },
-      {
-        highlight: "objective-c",
-        language: "objective-c",
-        logoClass: "objective-c",
-      },
-      {
-        highlight: "ocaml",
-        language: "ocaml",
-        logoClass: "ocaml",
-      },
-      {
-        highlight: "r",
-        language: "r",
-        logoClass: "r",
-      },
-      {
-        highlight: "swift",
-        language: "swift",
-        logoClass: "swift",
-      },
-      {
-        highlight: "kotlin",
-        language: "kotlin",
-        logoClass: "kotlin",
-      },
-      {
-        highlight: "rust",
-        language: "rust",
-        logoClass: "rust",
-      },
-    ],
+    languageTabs: languageTabs,
   } satisfies Preset.ThemeConfig,
   plugins: [
     [
