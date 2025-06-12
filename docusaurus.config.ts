608d43a4ad1a2e52af3864d2573a7e79b0d1f192
b-long <b-long@users.noreply.github.com>
2025-06-12 09:38:50 -0400

    Adding a comment, for testing purposes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,6 +18,7 @@ const config: Config = {
   favicon: "img/OpenTDF-Logo.png",
 
   // Set the production url of your site here
+  // Adding a comment, for testing purposes
   url: "https://docs.opentdf.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
