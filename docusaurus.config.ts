82f57656b21e1358d97b154c66b86582d514717e
Mary Dickson <mary.dickson@virtru.com>
2026-01-26 14:13:28 -0800

    Revert "add PR preview deploy to GH pages"
    This reverts commit b3ebc3de2e6742cdee183c5b5b35513af0f419cb.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -32,7 +32,7 @@ const config: Config = {
   url: "https://docs.opentdf.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.BASE_URL || "/",
+  baseUrl: "/",
   trailingSlash: false,
   customFields: {
     javaSdkVersion,
